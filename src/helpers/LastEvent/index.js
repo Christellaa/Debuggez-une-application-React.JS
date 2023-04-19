0c5528497e39c4db9fe30eda1001c9316fb1d0cc
Christella De Sousa <desousa.christella@proton.me>
2023-04-19 12:17:00 +0200

    Move place the code for better understanding and delete useless one

--- src/helpers/LastEvent/index.js
+++ src/helpers/LastEvent/index.js
@@ -1,14 +1,11 @@
 function GetLastEvent(events) {
-    const lastEvent = events[events.length - 1]
-
-    if (!events) return undefined;
-    // if there's no date, return last event of the array
-    if (!lastEvent?.date) return lastEvent;
-
-    // sort events in descending order of date, so the last event is always the most recent
-    events.sort((evtA, evtB) =>
-    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+  if (!events) return undefined;
+  
+  // sort events in descending order of date, so the last event is always the most recent
+  events.sort((evtA, evtB) =>
+  new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
+  
   // return the last event
   return events[0];
 }
