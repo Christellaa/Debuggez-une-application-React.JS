2884c50f409bba79dedc56ad91b697fce302086a
Christella De Sousa <desousa.christella@proton.me>
2023-05-03 08:43:08 +0200

    Remove tests for the Form as they shouldn't be in Home tests

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,32 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Home from "./index";
-
-describe("When Form is created", () => {
-  it("a list of fields card is displayed", async () => {
-    render(<Home />);
-    await screen.findByText("Email");
-    await screen.findByText("Nom");
-    await screen.findByText("Prénom");
-    await screen.findByText("Personel / Entreprise");
-  });
-
-  describe("and a click is triggered on the submit button", () => {
-    it("the success message is displayed", async () => {
-      render(<Home />);
-      fireEvent(
-        await screen.findByText("Envoyer"),
-        new MouseEvent("click", {
-          cancelable: true,
-          bubbles: true,
-        })
-      );
-      await screen.findByText("En cours");
-      await screen.findByText("Message envoyé !");
-    });
-  });
-
-});
-
+import EventCard from "../../components/EventCard/index";
 
 describe("When a page is created", () => {
   it("a list of events is displayed", () => {
@@ -36,20 +10,20 @@ describe("When a page is created", () => {
     const cards = screen.getByTestId("card-testid");
     expect(events).toBeInTheDocument();
     expect(cards).toBeInTheDocument();
-  })
+  });
   it("a list a people is displayed", () => {
     render(<Home />);
     const people = screen.getByTestId("people-testid");
     expect(people).toBeInTheDocument();
     expect(screen.getByText("Animateur"));
-  })
+  });
   it("a footer is displayed", () => {
     render(<Home />);
     const footer = screen.getByTestId("footer-testid");
     expect(footer).toBeInTheDocument();
     expect(screen.getByText("Notre derniére prestation"));
     expect(screen.getByText("Contactez-nous"));
-  })
+  });
   it("an event card, with the last event, is displayed", () => {
     render(
       <>
@@ -63,5 +37,5 @@ describe("When a page is created", () => {
     expect(lastCard.getAttribute("class").includes("Eventcard--small"));
     expect(lastImg).not.toBeNaN();
     expect(lastImg).toBeDefined();
-  })
+  });
 });
